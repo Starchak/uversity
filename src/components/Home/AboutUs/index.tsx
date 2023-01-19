fe42fff61e783f0b33479449e237f5bdcb8849b7
ostapmlnk88 <ostap.melnyk@outlook.com>
2023-01-18 20:05:08 -0500

    [add] recent update

--- src/components/Home/AboutUs/index.tsx
+++ src/components/Home/AboutUs/index.tsx
@@ -6,8 +6,10 @@ const AboutUs: FC = () => {
   return (
     <div className={styles.about_us}>
       <div className={styles.grey_block}></div>
-      <div className={styles.picture}></div>
-      <div className={styles.description}></div>
+      <div className={styles.overlay_box}>
+        <div className={styles.description}></div>
+      </div>
+
       <div className={styles.white_block}>
         {/* <div className={styles.title}></div>
         <div className={styles.subtitle}></div>
