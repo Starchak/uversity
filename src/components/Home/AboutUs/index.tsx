3e2c98ff4f8b48d8eb8427e19bf9f92b9da899ec
Ostap Melnyk <ostap.melnyk@outlook.com>
2023-01-23 14:13:08 -0500

    [add] - updated css

--- src/components/Home/AboutUs/index.tsx
+++ src/components/Home/AboutUs/index.tsx
@@ -1,20 +1,27 @@
 import React, { FC } from 'react';
-
+import Image from 'next/image';
 import styles from './index.module.scss';
 
 const AboutUs: FC = () => {
   return (
-    <div className={styles.about_us}>
+    <div className={styles.container}>
       <div className={styles.grey_block}></div>
-      <div className={styles.overlay_box}>
-        <div className={styles.description}></div>
+      <div className={styles.content}>
+        <div className={styles.picture}>
+          <Image
+            src="/about_us_1stBlock.png"
+            alt="About Us"
+            width={420}
+            height={575}
+          />
+          <div className={styles.info_block}>
+            Ми молода амбітна компанія, яка допомагає вирушити у пригоду на
+            навчання в омріяну країну світу.
+          </div>
+        </div>
+        <div className={styles.text}></div>
       </div>
-
-      <div className={styles.white_block}>
-        {/* <div className={styles.title}></div>
-        <div className={styles.subtitle}></div>
-        <div className={styles.btn}></div> */}
-      </div>
+      <div className={styles.white_block}></div>
     </div>
   );
 };
