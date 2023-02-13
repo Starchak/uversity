be9aa8fb719004c8de09bd29a81761425e9594d0
Maksym Maksymenko <imaksmaksimenko@gmail.com>
2023-02-13 12:27:12 -0500

    [add] - contact section to the slider

--- src/components/Home/Slider/index.tsx
+++ src/components/Home/Slider/index.tsx
@@ -1,9 +1,15 @@
 'use client';
 import React, { FC } from 'react';
+import Link from 'next/link';
 import Image from 'next/image';
 
 // Translation
 import { useTranslation } from '@i18n/client';
+
+// Icons
+import InstagramIcon from '/public/instagram_icon.svg';
+import FacebookIcon from '/public/fb_icon.svg';
+import YouTubeIcon from '/public/youtube_icon.svg';
 
 import styles from './index.module.scss';
 
@@ -28,7 +34,17 @@ const Slider: FC<SliderProps> = ({ lng }) => {
           <div className={styles.middle_line} />
           <h1 className={styles.title}>{t('slider_heading')}</h1>
         </div>
-        <div className={styles.right}></div>
+        <div className={`flex_center ${styles.right}`}>
+          <Link className={`flex_center ${styles.icon_wrap}`} href="#">
+            <InstagramIcon />
+          </Link>
+          <Link className={`flex_center ${styles.icon_wrap}`} href="#">
+            <FacebookIcon />
+          </Link>
+          <Link className={`flex_center ${styles.icon_wrap}`} href="#">
+            <YouTubeIcon />
+          </Link>
+        </div>
       </div>
     </section>
   );
